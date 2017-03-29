291aaf9c0c48237ce512ce5a1a51546447878f27
mikkonecny <mikkonecny@gmail.com>
2017-03-29 11:05:01 +0100

    Control.CollectErrors

--- src/Numeric/MixedTypes/Concept/CollectErrors.hs
+++ src/Numeric/MixedTypes/Concept/CollectErrors.hs
@@ -4,77 +4,7 @@ import Numeric.MixedTypes
 
 import Data.Monoid
 
-import Data.EnsureTypeOp
-
-{- Error-collecting machinery -}
-
-data CollectErrors es v =
-  CollectErrors (Maybe v) es
-  deriving (Show)
-
--- utilities:
-
-noErrors :: (Monoid es) => v -> CollectErrors es v
-noErrors v = CollectErrors (Just v) mempty
-
-prependErrors :: (Monoid es) => es -> CollectErrors es v -> CollectErrors es v
-prependErrors es1 (CollectErrors mv es2) = CollectErrors mv (es1 <> es2)
-
-firstNoErrors ::
-  (Monoid es)
-  =>
-  ((CollectErrors es a) -> (CollectErrors es b) -> t)
-  ->
-  (a -> (CollectErrors es b) -> t)
-firstNoErrors op (a :: a) (be :: CollectErrors es b) =
-  op (noErrors a :: CollectErrors es a) be
-
-secondNoErrors ::
-  (Monoid es)
-  =>
-  ((CollectErrors es a) -> (CollectErrors es b) -> t)
-  ->
-  ((CollectErrors es a) -> b -> t)
-secondNoErrors op (ae :: CollectErrors es a) (b :: b)  =
-  op ae (noErrors b :: CollectErrors es b)
-
--- functor instances:
-
-instance Functor (CollectErrors es) where
-  fmap f (CollectErrors mv es) =
-    CollectErrors (fmap f mv) es
-
-instance (Monoid es) => Applicative (CollectErrors es) where
-  pure = noErrors
-  (<*>) = zipCollectErrorsWith ($)
-
-zipCollectErrorsWith ::
-  (Monoid es) =>
-  (a -> b -> c) ->
-  (CollectErrors es a) -> (CollectErrors es b) -> (CollectErrors es c)
-zipCollectErrorsWith fn
-    (CollectErrors (Just a) ae) (CollectErrors (Just b) be) =
-        CollectErrors (Just (fn a b)) (ae <> be)
-zipCollectErrorsWith _
-    (CollectErrors _ ae) (CollectErrors _ be) =
-        CollectErrors Nothing (ae <> be)
-
-instance (Monoid es) => Monad (CollectErrors es) where
-  ae >>= f =
-    case ae of
-      CollectErrors (Just a) es ->
-        prependErrors es (f a)
-      CollectErrors _ es ->
-        CollectErrors Nothing es
-
-type CanEnsureCollectErrors es v = CanEnsureTypeOp (CollectErrors es) v
-type EnsureCollectErrors es v = EnsureTypeOp (CollectErrors es) v
-
-ensureCollectErrors :: (CanEnsureCollectErrors es v) => v -> EnsureCollectErrors es v
-ensureCollectErrors = ensureTypeOp
-
-instance (Monoid es) => CanEnsureTypeOp (CollectErrors es) Rational where
-  ensureTypeOp = noErrors
+import Control.CollectErrors
 
 {- Numeric exceptions -}
 
